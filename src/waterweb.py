588d73b1cf0514f28616cfbddfb93562f81bf908
Brenda Wallace <shiny@cpan.org>
2014-01-02 17:59:45 +1300

    writes the 1 and 0

--- src/waterweb.py
+++ src/waterweb.py
@@ -3,16 +3,27 @@ from bottle import route, run, template, static_file
 
 @route('/api/master/<action>', method='post')
 def master_switch(action):
-    if 'on' == action:
-        #turn on
-        pass
-    elif 'off' == action:
-        #turn off
-        pass
-    else:
-        raise RuntimeError("Unrecognised action: {action}".format(action=action))
-        
-    return []
+    print "opening the file"
+    f = open('mastervalve', 'w')   
+    
+    #open the files
+    try:
+         
+        if 'on' == action:
+            print "Writing a 1 to the file"
+            #turn on
+            f.write("1\n")
+            
+        elif 'off' == action:
+            print "Writing a zero to the file"
+            f.write("\n")
+        else:
+            raise RuntimeError("Unrecognised action: {action}".format(action=action))
+    except:
+        raise
+    finally: #everything in a finally runs, whether the above succeeds or not
+        print "closing the file"
+        f.close()
 
 @route('/')
 def index():
